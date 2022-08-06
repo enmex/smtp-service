e954b54ab125bef9388577545e421771533fa88a
ignavan39 <ivanignatenko28@gmail.com>
2022-08-06 07:50:37 +0300

    refactor config

--- config/config.go
+++ config/config.go
@@ -1,14 +1,18 @@
 package config
 
+type Credentials struct {
+	User     string `json:"user" yaml:"user"`
+	Password string `json:"password" yaml:"password"`
+}
+
+type Delivery struct {
+	Host    string `json:"host" yaml:"host"`
+	Address string `json:"address" yaml:"address"`
+}
+
 type Provider struct {
-	Credentials struct {
-		User     string `json:"user" yaml:"user"`
-		Password string `json:"password" yaml:"password"`
-	} `json:"credentials" yaml:"credentials"`
-	Delivery struct {
-		Host    string `json:"host" yaml:"host"`
-		Address string `json:"address" yaml:"address"`
-	} `json:"delivery" yaml:"delivery"`
+	Credentials Credentials `json:"credentials" yaml:"credentials"`
+	Delivery    Delivery    `json:"delivery" yaml:"delivery"`
 }
 
 var (
